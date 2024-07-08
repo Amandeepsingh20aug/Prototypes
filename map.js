b78ce416f958a49add695eb81010e043e4964ab4
amandeepsingh1929 <as0632568@gmail.com>
2024-07-09 02:58:47 +0530

    Written the custom Map function.

---
+++ map.js
@@ -0,0 +1,18 @@
+//Custom map Prototype.
+
+Array.prototype.myMap = function (cb) {
+  let temp = [];
+  for(let i=0 ; i< this.length;i++){
+    temp.push(cb(this[i],i,this))
+  }
+
+  return temp;
+}
+
+const nums = [1,2,3,4,5,6]
+
+const multiply  = nums.myMap((num,i,arr)=>{
+  return num * 3 ;
+})
+
+console.log(multiply);
